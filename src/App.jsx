fda48c92568fde46d9f1faa3a1a0ea59096e2cc2
Saidumarov <saidumarovjafarxon@gmail06.com>
2023-09-01 01:09:25 +0500

    tamom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/nav/Nav";
 import Layout from "./components/nav/Layout";
 import Chilpage from "./components/chila-bozor/Chilpage";
 
-
+const telegram = window.Telegram.WebApp;
 function App() {
+  useEffect(() =>{
+    telegram.ready();
+  })
   return (
     <div className="app" >
  
